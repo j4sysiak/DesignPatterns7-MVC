c078d5755bcb6807c28efa5a990d9a0585b18ed6
j4sysiak <j4sysiak@gmail.com>
2019-02-21 07:29:28 +0100

    asda

---
+++ DesignPatterns7 - MVC/src/pl/jaceksysiak/designpatterns/demo1/controller/Controller.java
@@ -0,0 +1,16 @@
+package pl.jaceksysiak.designpatterns.demo1.controller;
+
+import pl.jaceksysiak.designpatterns.demo1.model.Model;
+import pl.jaceksysiak.designpatterns.demo1.view.View;
+
+public class Controller {
+	private View view;
+	private Model model;
+	
+	public Controller(View view, Model model) {
+		this.view = view;
+		this.model = model;
+	}
+	
+	
+}
